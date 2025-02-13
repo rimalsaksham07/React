7e3945e8a0160ea9560c518136089a80dedac548
rimalsaksham07 <sakshamo345@gmail.com>
2025-02-13 21:14:29 +0545

    new commit

--- 06passwordgenerator/src/App.jsx
+++ 06passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback ,useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -6,31 +6,34 @@ function App() {
   const [characterAllowed, setcharacterAllowed] = useState(false);
   const [password, setPassword] = useState(" ");
 
+  const passwordRef = useRef(null);
+
   const passwordGenerator = useCallback(() => {
-    let count = 0
+    let count = 0;
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "0123456789";
     if (characterAllowed) str += "!@#$%^&*()";
 
     for (let i = 0; i < length - 1; i++) {
-      let char = Math.floor(Math.random() *( str.length-1)+ 1);
+      let char = Math.floor(Math.random() * (str.length - 1) + 1);
       console.log(char);
-      count = count + 1
-      
+      count = count + 1;
       pass = pass + str[char];
     }
-    
+
     setPassword(pass);
-  }, [length, numberAllowed, characterAllowed, setPassword]);
+  }, [length, numberAllowed, characterAllowed, setPassword]); // aba esko kei dependecy change garyo bhane function ni change hune bhayo
+
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select(); // elle copy garda text lai bold garauxa
+    passwordRef.current?.setSelectionRange(0, 3); // elle chai range dinxa kati copy garne bhanera 
+    window.navigator.clipboard.writeText(pass); // elle chai copy garxa
+  }, [password]);
 
   useEffect(() => {
-    passwordGenerator()
-
-  } , [length , numberAllowed , characterAllowed ,passwordGenerator])
-
-
-
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
 
   return (
     <>
@@ -39,18 +42,25 @@ function App() {
         <div className="flex shadow rounded-3xl overflow-hidden mb-4">
           <input
             type="text"
-            value={password}
+            value={password} // this is out evaluated expression hamle expression lekhne ho function haina thats why it is evaluated expression
             className="outline-none w-full py-1 px-3 bg-amber-50 text-black"
             placeholder="password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="py-3 rounded-2xl text-amber-300 px-1">Copy</button>
+
+          <button
+            className="py-3 rounded-2xl text-amber-300 px-1 "
+            onClick={copyPassword}
+          >
+            Copy
+          </button>
         </div>
         <div className="flex text-sm gap-x-2 ">
           <div className="flex items-center gap-x-1">
             <input
               type="range"
-              min={6}
+              min={8}
               max={100}
               value={length}
               className="cursor-pointer"
@@ -66,7 +76,7 @@ function App() {
               type="checkbox"
               defaultChecked={numberAllowed}
               id="numberInput"
-              onChange={() => {
+              onClick={() => {
                 setNumberAllowed((prev) => !prev);
               }}
             />
