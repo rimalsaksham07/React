498e74063bfaf7accfe10e323772d4a8b1313903
rimalsaksham07 <sakshamo345@gmail.com>
2025-02-14 21:34:18 +0545

    new commit

--- 01practice/02Practice/src/App.jsx
+++ 01practice/02Practice/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
   return (
     <>
       <div className="flex bg-green-200 justify-center items-center h-screen text-6xl">
-        <div className="bg-green-300 rounded-4xl w-20 h-40 flex justify-center items-center place-items-stretch">
+        <div className="bg-green-300 rounded-4xl w-20 h-40 flex justify-center items-center place-items-stretch hover:bg-green-400">
           <button onClick={sub}>-</button>
         </div>
         <div className="bg-green-400 rounded-4xl w-90 h-40 flex justify-center items-center">
           Count : {count}
         </div>
-        <div className="bg-green-300 rounded-4xl w-20 h-40 flex justify-center items-center">
+        <div className="bg-green-300 rounded-4xl w-20 h-40 flex justify-center items-center place-items-stretch hover:bg-green-400">
           <button onClick={add}>+</button>
         </div>
       </div>
