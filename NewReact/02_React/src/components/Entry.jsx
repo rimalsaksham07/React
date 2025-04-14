4b84c575f0233cbe8df575e93165388696851388
rimalsaksham07 <sakshamo345@gmail.com>
2025-04-14 21:50:06 +0545

    new commit

---
+++ NewReact/02_React/src/components/Entry.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+function Entry() {
+  return (
+    <div className="entry-container">
+      <div className="struct">
+        <img
+          src="src/assets/fuji.avif"
+          alt=""
+          srcset=""
+          height={200}
+          width={200}
+        />
+        <div className="loc">
+          <div className="intro">
+            <p>Japan</p>
+            <a href=""> View on google Map</a>
+          </div>
+          <h1>Mount Fuji</h1>
+          <h5>12 Jan 2021 - 24 Jan 2021</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            repellendus laborum esse quis corporis officiis. Illo eius itaque
+            alias laboriosam.
+          </p>
+        </div>
+      </div>
+      <div className="struct">
+        <img
+          src="src/assets/fuji.avif"
+          alt=""
+          srcset=""
+          height={200}
+          width={200}
+        />
+        <div className="loc">
+          <div className="intro">
+            <p>Japan</p>
+            <a href=""> View on google Map</a>
+          </div>
+          <h1>Mount Fuji</h1>
+          <h5>12 Jan 2021 - 24 Jan 2021</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            repellendus laborum esse quis corporis officiis. Illo eius itaque
+            alias laboriosam.
+          </p>
+        </div>
+      </div>
+      <div className="struct">
+        <img
+          src="src/assets/fuji.avif"
+          alt=""
+          srcset=""
+          height={200}
+          width={200}
+        />
+        <div className="loc">
+          <div className="intro">
+            <p>Japan</p>
+            <a href=""> View on google Map</a>
+          </div>
+          <h1>Mount Fuji</h1>
+          <h5>12 Jan 2021 - 24 Jan 2021</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            repellendus laborum esse quis corporis officiis. Illo eius itaque
+            alias laboriosam.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Entry;
---
+++ NewReact/02_React/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <header className='header'>
+        <img src="src/assets/download.png" alt="" srcset=""height={30}/>
+        <p>my travel journal</p>
+        </header>   
+    </div>
+  )
+}
+
+export default Header
